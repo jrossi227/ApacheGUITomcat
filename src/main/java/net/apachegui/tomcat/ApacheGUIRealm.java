768a7b80f572d1fcd91329668d7ba92ac88cb736
jrossi227 <jonathan.m.rossi@gmail.com>
2015-04-20 16:43:06 -0400

    Override JDBCRealm to enable no authentication mode

--- src/main/java/net/apachegui/tomcat/ApacheGUIRealm.java
+++ src/main/java/net/apachegui/tomcat/ApacheGUIRealm.java
@@ -1,8 +1,22 @@
 package net.apachegui.tomcat;
 
-/**
- * Created by jonathanrossi on 15-04-20.
- */
-public class ApacheGUIRealm
+import net.apachegui.db.SettingsDao;
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Request;
+import org.apache.catalina.deploy.SecurityConstraint;
+import org.apache.catalina.realm.JDBCRealm;
+
+public class ApacheGUIRealm extends JDBCRealm
 {
+    @Override
+    public SecurityConstraint[] findSecurityConstraints(Request request, Context context) {
+        String enabled = SettingsDao.getInstance().getSetting("authenticationEnabled");
+        if(enabled != null && enabled.equals("false")) {
+            return null;
+        }
+
+        return super.findSecurityConstraints(request, context);
+    }
+
+
 }
